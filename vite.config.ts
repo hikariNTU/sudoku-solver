b846644e073838721e9033be1cb89067d54bb3d7
hikariNTU <18393696+hikariNTU@users.noreply.github.com>
2023-06-09 15:41:00 +0800

    feat: publish page to GH page

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/sudoku-solver/',
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) }],
